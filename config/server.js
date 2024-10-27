ad7a5c0ef71e99ca0164353a6442713eb2bbca9c
Boris Schauer <me@bschauer.de>
2024-10-27 11:18:44 +0100

    Try host and port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: process.env.HOST || '0.0.0.0',
-  port: process.env.PORT || 1337,
+  host: env("HOST") || '0.0.0.0',
+  port: env("PORT") || 1337,
   app: {
     keys: env.array('APP_KEYS'),
   },
