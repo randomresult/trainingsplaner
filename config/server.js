211ca09ba0d93af75fa37809a8079d5dc9b2d3c5
Harry Glatz <glatz@analog.de>
2024-11-03 08:19:33 +0100

    use https

--- config/server.js
+++ config/server.js
@@ -1,10 +1,17 @@
+// config/server.js
 module.exports = ({ env }) => ({
-  host: env("HOST") || '0.0.0.0',
-  port: env("PORT") || 1337,
+  host: '0.0.0.0',
+  port: env.int('PORT', 1337),
+  url: env('NODE_ENV') === 'production'
+      ? env('PUBLIC_URL')
+      : undefined,  // Im Entwicklungsmodus standard URL verwenden
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+  proxy: true,
 });
