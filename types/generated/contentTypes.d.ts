601a8006405caf50eeeb74b8a49724db5cf93582
Harry Glatz <glatz@analog.de>
2024-11-17 07:10:22 +0100

    Add Video Url

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -474,6 +474,7 @@ export interface ApiExerciseExercise extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    VideoUrl: Schema.Attribute.String;
   };
 }
 
